bee35e92dc166891de3ab24ff8f6f4bd8d907c81
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-05-26 10:09:39 +0600

    update profile api implemented

--- src/modules/profile/services/updateProfile.ts
+++ src/modules/profile/services/updateProfile.ts
@@ -1,15 +1,29 @@
+import { JWT_SECRET } from '../../../config';
 import { TUser } from '../../users/user.interface';
 import { UserModel } from '../../users/user.model';
 import { TUpdateProfilePayload } from '../profile.validation';
+import jwt from 'jsonwebtoken';
 
 export const updateProfile = async (
   user: TUser,
-  payload: TUpdateProfilePayload,
+  payload: TUpdateProfilePayload
 ) => {
-  const updatedUser = await UserModel.updateOne(
+  const updatedUser = await UserModel.findOneAndUpdate(
     { _id: user._id },
     { $set: payload },
+    { new: true }
   );
 
-  return updatedUser;
+  const token = jwt.sign(
+    {
+      _id: updatedUser?._id,
+      email: updatedUser?.email,
+      name: updatedUser?.name,
+      role: updatedUser?.role,
+    },
+    JWT_SECRET!,
+    { expiresIn: '30d' }
+  );
+
+  return { token };
 };

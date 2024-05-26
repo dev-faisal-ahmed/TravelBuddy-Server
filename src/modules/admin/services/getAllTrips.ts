de033bc20d5dd95a60ee7a0252bdebc7d88925e4
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-05-26 13:06:30 +0600

    all trips for admin api updated

--- src/modules/admin/services/getAllTrips.ts
+++ src/modules/admin/services/getAllTrips.ts
@@ -1,6 +1,9 @@
 import { TripModel } from '../../trip/trip.model';
 
 export const getAllTrips = async () => {
-  const trips = await TripModel.find();
+  const trips = await TripModel.find().populate({
+    path: 'user',
+    select: 'name',
+  });
   return trips;
 };

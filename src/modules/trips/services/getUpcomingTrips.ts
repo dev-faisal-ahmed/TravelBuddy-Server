fabcdcc4079801dd8c23b0a9d7e30985a6c54928
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-08-15 16:10:35 +0600

    get upcoming trips api updated

--- src/modules/trips/services/getUpcomingTrips.ts
+++ src/modules/trips/services/getUpcomingTrips.ts
@@ -4,6 +4,6 @@ export const getUpComingTrips = async () => {
   const today = new Date();
   today.setHours(0, 0, 0);
 
-  const trips = await TripModel.find({ startDate: { $gte: today } });
+  const trips = await TripModel.find({ startDate: { $gte: today } }).limit(6);
   return trips;
 };
